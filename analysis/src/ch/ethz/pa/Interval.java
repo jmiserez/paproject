98678607e42cf00e77864b16384f6e78ed0515ac
Erik Henriksson <erik.henriksson@me.com>
2013-11-18 21:30:22 +0100

    Refactored Interval the correct way

--- analysis/src/ch/ethz/pa/Interval.java
+++ analysis/src/ch/ethz/pa/Interval.java
@@ -1,6 +1,19 @@
 package ch.ethz.pa;
 
+import ch.ethz.pa.pair.Pair;
+import ch.ethz.pa.pair.PairEq;
+
 public class Interval {
+	// TODO: Do you need to handle infinity or empty interval?
+	private final static int INF = 10;
+	
+	protected int lower, upper;
+	protected boolean bot = false;
+	
+	public Interval() {
+		bot = true;
+	}
+	
 	public Interval(int start_value) {
 		lower = upper = start_value;
 	}
@@ -16,13 +29,9 @@ public class Interval {
 	}
 	
 	public void copyFrom(Interval other) {
-		lower = other.lower;
-		upper = other.upper;
-	}
-	
-	public static Interval plus(Interval i1, Interval i2) {
-		// TODO: Handle overflow. 
-		return bounded(new Interval(i1.lower + i2.lower, i1.upper + i2.upper));
+		Interval i = (Interval) other;
+		lower = i.lower;
+		upper = i.upper;
 	}
 
 	private static Interval bounded(Interval i) {
@@ -30,33 +39,7 @@ public class Interval {
 		i.upper = Math.min(i.upper, INF);
 		return i;
 	}
-
-	public static Interval minus(Interval i1, Interval i2) {
-		// TODO: Handle overflow. 
-		return bounded(new Interval(i1.lower - i2.lower, i1.upper - i2.upper));
-	}
-
-	public static Interval multiply(Interval i1, Interval i2) {
-		// TODO: Handle overflow.
-		int newLower = i1.lower * i2.lower, 
-			newUpper = i1.upper * i2.upper;
-		// To handle case [-1, 1] * [-1, 1]
-		if (i1.lower < 0 && i2.lower < 0 && (i1.upper >= 0 || i2.upper >= 0))
-			newLower *= -1;
-		// To handle case [-2, -1] * [-2, -1]
-		if (i1.upper < 0 && i2.upper < 0 && (i1.upper >= 0 || i2.upper >= 0))
-			newUpper *= -1;
-		return bounded(new Interval(newLower, newUpper));
-	}
 	
-	public static Interval join(Interval i1, Interval i2) {
-		if (i1 == null) // BOT
-			return i2;
-		if (i2 == null) // BOT
-			return i1;
-		return bounded(new Interval(Math.min(i1.lower, i2.lower), Math.max(i1.upper, i2.upper)));
-	}
-
 	private void set(int min, int max) {
 		this.lower = min;
 		this.upper = max;
@@ -77,10 +60,65 @@ public class Interval {
 		return lower > upper;
 	}
 
-	// TODO: Do you need to handle infinity or empty interval?
-	int lower, upper;
+	public Interval plus(Interval a) {
+		// Cross fingers and hope a is instanceof Interval
+		Interval i = (Interval) a;
+		// TODO: Handle overflow. 
+		return bounded(new Interval(this.lower + i.lower, this.upper + i.upper));
+	}
+
+	public Interval minus(Interval a) {
+		// Cross fingers and hope a is instanceof Interval
+		Interval i = (Interval) a;
+		// TODO: Handle overflow. 
+		return bounded(new Interval(this.lower - i.lower, this.upper - i.upper));
+	}
+
+	public Interval multiply(Interval a) {
+		// Cross fingers and hope a is instanceof Interval
+		Interval i = (Interval) a;
+		// TODO: Handle overflow.
+		int newLower = this.lower * i.lower, 
+			newUpper = this.upper * i.upper;
+		// To handle case [-1, 1] * [-1, 1]
+		if (this.lower < 0 && i.lower < 0 && (this.upper >= 0 || i.upper >= 0))
+			newLower *= -1;
+		// To handle case [-2, -1] * [-2, -1]
+		if (this.upper < 0 && i.upper < 0 && (this.upper >= 0 || i.upper >= 0))
+			newUpper *= -1;
+		return bounded(new Interval(newLower, newUpper));
+	}
+
+	public Interval join(Interval a) {
+		// Cross fingers and hope a is instanceof Interval
+		Interval i = (Interval) a;
+		if (this.equals(getBot()))
+			return i;
+		if (i.equals(getBot()))
+			return this;
+		return bounded(new Interval(Math.min(this.lower, i.lower), Math.max(this.upper, i.upper)));
+	}
+
+	public Interval meet(Interval a) {
+		// Cross fingers and hope a is instanceof Interval
+		Interval i = (Interval) a;
+		if (this.equals(getBot()))
+			return getBot();
+		if (i.equals(getBot()))
+			return getBot();
+		if (this.lower > i.upper || i.lower > this.upper)
+			return null;
+		return new Interval(Math.max(this.lower, i.lower), Math.min(this.upper, i.upper));
+	}
+
+	public Interval getTop() {
+		return new Interval(-INF, INF);
+	}
+
+	public Interval getBot() {
+		return new Interval();
+	}
 	
-	public final static int INF = 10;
-	public final static Interval TOP = new Interval(-INF, INF);
-	public final static Interval BOT = null;
+	
+	
 }
