f41684d1861aeea86a464895f46f194a4d3dcde5
Jeremie Miserez <jeremie@miserez.org>
2013-11-19 16:26:13 +0100

    add ProgramIsunsafeException and ReadValueWithoutAssignment

---
+++ analysis/src/ch/ethz/pa/ProgramIsUnsafeException.java
@@ -0,0 +1,14 @@
+package ch.ethz.pa;
+
+@SuppressWarnings("serial")
+public class ProgramIsUnsafeException extends RuntimeException {
+	
+	public ProgramIsUnsafeException() {
+		super();
+	}
+	public ProgramIsUnsafeException(String reason) {
+		super(reason);
+	}
+	
+
+}
